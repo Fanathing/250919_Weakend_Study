d62690d1dd9d39dcf8de0bfba73897a93476c589
Fanathing <mmybonnie@gamil.com>
2025-09-19 18:47:40 +0900

    Feat: modify post 요청시 수정처리 및 view.html에서 delete요청시 삭제되도록 처리

--- controllers/board.controller.js
+++ controllers/board.controller.js
@@ -1,6 +1,11 @@
 const boards = require("../db/boards.data.js");
 const path = require("path");
 
+// 생성일을 선언
+const now = new Date();
+const date = `${now.getFullYear()}/${now.getMonth()+1}/${now.getDate()}`
+// 유니크 키를 선언
+
 
 // get 요청으로 게시글 목록을 응답하는 역할
 const getList = (req, res) => {
@@ -17,10 +22,7 @@ const getCreate = (req, res) => {
 // post 요청으로 게시글을 생성하는 역할
 const postCreate = (req, res) => {
     const { userId, title, content } = req.body;
-    // 생성일을 선언
-    const now = new Date();
-    const date = `${now.getFullYear()}/${now.getMonth()+1}/${now.getDate()}`
-    // 유니크 키를 선언
+
     const key =  Date.now();
 
     boards.push({
@@ -47,14 +49,46 @@ const getView = (req, res) => {
     
 }
 
-const getModify = (req,res) => {
+const getModify = (req, res) => {
     const { key } = req.params
     
     const board = boards.find((board) => board.key == key)
     res.render("boards/modify.html", {
         board
     })
-    console.log(board);
+}
+
+const postModify = (req, res) => {
+    const { key } = req.params
+    const { userId, title, content } = req.body
+    boards.forEach((board) => {
+
+        if (board.key == key) {
+            board.userId = userId
+            board.title = title
+            board.content = content
+            board.updated_at = `${date}업뎃함`
+        }
+    })
+    res.redirect("/boards");
+}
+    // for(let i = 0; i < replyData.length; i++) {
+    //    if(replyData[i].replyId === replyId) {
+    //         replyData.splice(replyIndex, 1);
+    //         break;
+    //    }
+
+    // }
+
+const getDelete = (req, res) => {
+    const { key } = req.params
+    for (let i = 0; i < boards.length; i++) {
+        if (boards[i].key == key) {
+        boards.splice(i, 1);
+        break;
+        }
+    }
+    res.redirect("/boards");
 }
 
 module.exports = {
@@ -62,5 +96,7 @@ module.exports = {
     getCreate,
     postCreate,
     getView,
-    getModify
+    getModify,
+    postModify,
+    getDelete
 }
--- router/board.router.js
+++ router/board.router.js
@@ -5,8 +5,10 @@ const router = express.Router();
 router.get("/", boardController.getList);
 router.get("/create", boardController.getCreate);
 router.post("/create", boardController.postCreate);
-router.get("/view/:key", boardController.getView)
-router.get("/modify/:key", boardController.getModify)
+router.get("/view/:key", boardController.getView);
+router.get("/modify/:key", boardController.getModify);
+router.post("/modify/:key", boardController.postModify);
+router.get("/delete/:key", boardController.getDelete);
 
 module.exports = router;
 
